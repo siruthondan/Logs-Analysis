9d5855323404d3e4f5b7c78f90954da03c106fc8
siruthondan <siruthondan@gmail.com>
2019-08-21 14:09:10 +0200

    shebang and PEP8 changes as per the review comments.

---
+++ ReportingTool_v1.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+
+import psycopg2
+
+try:
+
+    query1 = """SELECT title, count(log.id) as views
+            FROM articles, log
+            WHERE log.path = CONCAT(\'/article/\', articles.slug)
+            GROUP BY articles.title ORDER BY views desc Limit 3;"""
+    query2 = """SELECT authors.name,count(authors.id) as Views
+            FROM articles, authors, log
+            WHERE log.path = CONCAT(\'/article/\', articles.slug)
+            and articles.author = authors.id
+            Group By authors.name,authors.id Order By Views Desc;"""
+    query3 = """SELECT to_char(time,\'DD-MON-YYYY\'),
+            COUNT(to_char(time,\'DD-MON-YYYY\')) as count
+            FROM log
+            GROUP BY to_char(time,\'DD-MON-YYYY\')
+            ORDER BY count desc;"""
+    query4 = """SELECT to_char(time,\'DD-MON-YYYY\'),
+            COUNT(to_char(time,\'DD-MON-YYYY\')) as count
+            FROM log
+            WHERE Status = \'404 NOT FOUND\'
+            GROUP BY to_char(time,\'DD-MON-YYYY\')
+            ORDER BY count desc;"""
+
+    conn = psycopg2.connect("dbname='news'")
+    curr = conn.cursor()
+
+    print(' ')
+    print('Top 3 Popular Articles:')
+    curr.execute(query1)
+    rows = curr.fetchall()
+    for row in rows:
+        print('\t' + row[0] + ' - ' + str(row[1]) + ' views')
+
+    print(' ')
+    print 'Most popular authors of all time:'
+    curr.execute(query2)
+    rows = curr.fetchall()
+    for row in rows:
+        print('\t' + row[0] + ' - ' + str(row[1]) + ' views')
+
+    print(' ')
+    print('Total error requests count on a day')
+    curr.execute(query3)
+    logcount = curr.fetchall()
+    curr.execute(query4)
+    rows = curr.fetchall()
+    for row in rows:
+        for logg in logcount:
+            if row[0] == logg[0]:
+                percen = round(100.0 * row[1] / logg[1], 2)
+                if percen > 1:
+                    print(
+                            '\t' + row[0] + ' - ' + str(row[1]) +
+                            ' Bad Requests' + ' - ' +
+                            str(percen) + ' Percentage'
+                    )
+    print(' ')
+except (Exception, psycopg2.Error), error:
+    print ('Error while connecting to PostgreSQL', error)
+finally:
+    if conn:
+        curr.close()
+        conn.close()
